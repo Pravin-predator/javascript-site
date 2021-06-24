494769700a9b0fcf466cd907581172d39ba3f680
Pravin-predator <61304004+Pravin-predator@users.noreply.github.com>
2021-06-24 20:17:21 +0530

    Update example.py

--- selenium/spider/example.py
+++ selenium/spider/example.py
@@ -1,25 +1,22 @@
 import scrapy
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 
-class ExampleSpider(scrapy.Spider):
-    name = 'example'
+class CountriesSpiderSpider(scrapy.Spider):
+    name = "countries_spider"
     def start_requests(self):
-        url = 'https://quotes.toscrape.com/scroll/'
+        url = "https://quotes.toscrape.com/scroll"
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         driver = webdriver.Chrome()
-        driver.get('https://quotes.toscrape.com/scroll/')
-        driver.implicitly_wait(10)
-        quotes = driver.find_elements_by_css_selector("div.quote")
-        for qt in quotes:
-            yield{
-                'text': driver.find_element_by_css_selector('span.text::text').get()
+        driver.get("https://quotes.toscrape.com/scroll")
+        quote = driver.find_elements_by_class_name("quote")
+        for country in quote:
+            yield {
+                "quote": country.text
             }
 
 
-
-
+        driver.quit()
